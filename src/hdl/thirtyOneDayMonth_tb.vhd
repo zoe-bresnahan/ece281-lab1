b45e3584e891f8c473dd5cc77818bf45fe6b3a2b
roleohibachi <roleohibachi@gmail.com>
2024-01-30 20:46:28 -0700

    reverse bit order in tb

--- src/hdl/thirtyOneDayMonth_tb.vhd
+++ src/hdl/thirtyOneDayMonth_tb.vhd
@@ -76,11 +76,11 @@ begin
 	-- PORT MAPS ----------------------------------------
 	-- map ports for any component instances (port mapping is like wiring hardware)
     thirtyOneDayMonthMux_inst : thirtyOneDayMonth port map (
-			i_A => w_sw(3),
-			i_B => w_sw(2),
-			i_C => w_sw(1),
-			i_D => w_sw(0),
-			o_Y => w_Y
+			i_D => w_sw(3),
+			i_C => w_sw(2),
+			i_B => w_sw(1),
+			i_A => w_sw(0),
+	    		o_Y => w_Y
         );
 	-----------------------------------------------------
 
